fcb8b95be8c58978f376d9d35fb286482fa61d2b
injifann <gutemaurgawa@gmail.com>
2026-06-22 12:44:53 +0300

    updated generete sku function

---
+++ backend/utilities/generateUniqueSKU.js
@@ -0,0 +1,22 @@
+import Product from "../models/productModel.js";
+
+export const generateUniqueSKU = async (category,productTitle) => {
+
+  const categoryPrefix = category.substring(0, 3).toUpperCase();
+
+
+  const productPrefix = productTitle.trim().replace(/[^a-zA-Z]/g, "").substring(0, 3).toUpperCase();
+
+  let sku;
+  let exists = true;
+
+  while (exists) {
+    const randomPart = Math.random().toString(36).substring(2, 8).toUpperCase();
+
+    sku = `${categoryPrefix}-${productPrefix}-${randomPart}`;
+
+    exists = await Product.exists({ sku });
+  }
+
+  return sku;
+};
