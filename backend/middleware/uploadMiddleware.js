ecaafec63fefafda9a0c3e4fea7344d09d8fa352
injifann <gutemaurgawa@gmail.com>
2026-06-23 08:39:28 +0300

    configure multer for secure file uploads

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -1,7 +1,14 @@
 
 import multer from "multer";
 
-const storage = multer.diskStorage({});
+const storage = multer.diskStorage({
+    destination:(req,file,cb) =>{
+        cb(null,"uploads/");
+    },
+    filename: (req,file,cb)=>{
+        cb(null,Date.now() + path.extname(file.originalname));
+    }
+});
 
 const upload = multer({
     storage,
