bee2b0c93c2351e418af73d047c1b50bf4292e8f
injifann <gutemaurgawa@gmail.com>
2026-06-23 10:17:34 +0300

    fixed bug

--- backend/utilities/sendAuthResponse.js
+++ backend/utilities/sendAuthResponse.js
@@ -3,7 +3,7 @@ import { generateToken } from "./generateToken.js"
 export const sendAuthResponse = async (res,statusCode,message , user)=>
 {
     const token = generateToken(user._id);
-    res.status(statusCode).json({
+    return res.status(statusCode).json({
         message:message,
         status:statusCode,
         token:token,
