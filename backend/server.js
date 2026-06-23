026805dc5e1b0b9d6e385a7c0ad103a8954fb276
injifann <gutemaurgawa@gmail.com>
2026-06-23 08:47:51 +0300

    imported router to use in app

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,20 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js'
 import { errorHandler } from './middleware/errorHandler.js';
+import userRouter from './routes/userRoutes.js'
+import adminRoutes from './routes/adminRoutes.js'
 
 
 const app = express();
 dotenv.config();
-const port=process.env.PORT || 5000;
+const port=process.env.PORT || 5001;
 app.use(express.json());
+app.use("/api/user",userRouter);
+app.use("/admin",adminRoutes);
 app.use(errorHandler);
 
 connectDB();
 app.listen(port,()=>{
     console.log("the server is running on",port ," port")
 })
+
