b3e2c6a96bcbf0945e290b146c5ff747c7df513a
injifann <gutemaurgawa@gmail.com>
2026-06-25 09:37:00 +0300

    added new routes

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -3,12 +3,18 @@ import express from 'express';
 import { admin,protect } from '../middleware/protect.js';
 import { addProduct,updateProduct, deleteProduct } from '../controllers/adminController.js';
 import upload from '../middleware/uploadMiddleware.js';
-
+import { addCategory,deleteCategory,updateCategory ,getAllCategory,getCategoryById } from '../controllers/categoryController.js';
 
 const router = new express.Router();
 
-router.post("/addproduct",protect, admin,upload.single("image"),addProduct);
-router.post("/updateproduct",protect,admin,upload.single("image"),updateProduct);
+router.post("/addproduct",protect, admin,upload.array("images"),addProduct);
+router.post("/updateproduct",protect,admin,upload.array("images"),updateProduct);
 router.post("/deleteproduct",protect,admin,deleteProduct);
+router.post("/addcategory",protect,admin,addCategory);
+router.post("/deletecategory",protect,admin,deleteCategory);
+router.post("/updatecategory",protect,admin,updateCategory);
+
+
+
 
 export default router
