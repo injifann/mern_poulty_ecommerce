ac356f0bfa91705e0e81974b68cbf72e8a160cfd
injifann <gutemaurgawa@gmail.com>
2026-06-30 06:49:56 +0300

    included cors

--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,17 @@ import { errorHandler } from './middleware/errorHandler.js';
 import userRouter from './routes/userRoutes.js'
 import adminRoutes from './routes/adminRoutes.js'
 import productRoute from './routes/productRoutes.js'
-
-
+import cors from 'cors';
 
 const app = express();
 dotenv.config();
 const port=process.env.PORT || 5001;
 app.use(express.json());
+app.use(cors());
 app.use("/uploads", express.static("uploads"));
 app.use("/api/user",userRouter);
 app.use("/api/admin",adminRoutes);
-app.use("api/product/",productRoute);
+app.use("/api/product/",productRoute);
 app.use(errorHandler);
 
 connectDB();
