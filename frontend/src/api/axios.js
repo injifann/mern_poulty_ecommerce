00d80c9fe02e66dff98dcd6de9a51b12bb67696d
injifann <gutemaurgawa@gmail.com>
2026-07-01 21:22:30 +0300

    frontend :created axios instance

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const api = import.meta.env.VITE_API_URL;
+
+const axiosInstance = axios.create({
+  baseURL: api,
+});
+
+// 🔐 Interceptor → automatically attach token
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
