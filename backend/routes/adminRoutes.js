8dc790562f13436c38dd2149be3835a4dff4852c
injifann <gutemaurgawa@gmail.com>
2026-07-05 07:47:26 +0300

    backend-fixed:included newly created end point

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,20 +1,19 @@
 
 import express from 'express';
 import { admin,protect } from '../middleware/protect.js';
-import { addProduct,updateProduct, deleteProduct } from '../controllers/adminController.js';
+import { addProduct,updateProduct, deleteProduct,getstats } from '../controllers/adminController.js';
 import upload from '../middleware/uploadMiddleware.js';
 import { addCategory,deleteCategory,updateCategory ,getAllCategory,getCategoryById } from '../controllers/categoryController.js';
 
 const router = new express.Router();
 
 router.post("/addproduct",protect, admin,upload.array("images"),addProduct);
-router.post("/updateproduct",protect,admin,upload.array("images"),updateProduct);
-router.post("/deleteproduct",protect,admin,deleteProduct);
+router.put("/updateproduct",protect,admin,upload.array("images"),updateProduct);
+router.post("/deleteproduct/:id",protect,admin,deleteProduct);
 router.post("/addcategory",protect,admin,addCategory);
 router.post("/deletecategory",protect,admin,deleteCategory);
 router.post("/updatecategory",protect,admin,updateCategory);
-
-
+router.get("/getstats",protect,admin,getstats);
 
 
 export default router
