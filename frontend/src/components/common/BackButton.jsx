ea0af7d3c4dc34487a8148f4366811614f591452
injifann <gutemaurgawa@gmail.com>
2026-07-10 13:03:46 +0300

    frontend-feat:created reusable button component

---
+++ frontend/src/components/common/BackButton.jsx
@@ -0,0 +1,16 @@
+import { useNavigate } from "react-router";
+import React from 'react'
+import { FaArrowLeft } from "react-icons/fa";
+export default function backButton({to,text= "back"}) {
+    const navigate = useNavigate();
+
+  return (
+    <div className="flex justify-center mt-8">
+    <button onClick={()=>navigate(to)}className="flex items-center gap-2 px-4 py-2 bg-gray-800 text-white rounded-lg shadow hover:bg-gray-700 transition"
+>
+     <FaArrowLeft />
+      {text}
+    </button>
+     </div>
+  )
+}
