86fe8677c7a40a55b3d80e92657da017b0a648a8
injifann <gutemaurgawa@gmail.com>
2026-07-11 18:34:55 +0300

    backend-fix:removed middleware to get category inside category route

--- backend/routes/categoryRoute.js
+++ backend/routes/categoryRoute.js
@@ -6,8 +6,8 @@ const router = new express.Router();
 router.post("/addcategory",protect,admin,addCategory);
 router.delete("/deletecategory/:id",protect,admin,deleteCategory);
 router.put("/updatecategory/:id",protect,admin,updateCategory);
-router.get("/getallcategory",protect,admin,getAllCategory);
-router.get("/getcategorybyid/:id",protect,admin,getCategoryById);
+router.get("/getallcategory",getAllCategory);
+router.get("/getcategorybyid/:id",getCategoryById);
 
 export default router
 
