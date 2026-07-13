961f67e4d616afc4775271c133ac53e3fd841583
injifann <gutemaurgawa@gmail.com>
2026-07-13 18:05:14 +0300

    backend:removed unused route from cart route

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getMyCart,addToCart,deleteCart,RemoveProductFromCart,updateCartQuantity,updateCart } from '../controllers/cartController.js';
+import { getMyCart,addToCart,deleteCart,RemoveProductFromCart,updateCart } from '../controllers/cartController.js';
 import { protect } from '../middleware/protect.js';
 
 const router = new express.Router();
@@ -8,7 +8,6 @@ router.get("/getmycart",protect,getMyCart);
 router.post("/addtocart",protect,addToCart);
 router.delete("/removefromcart",protect,RemoveProductFromCart)
 router.delete("/deletecart",protect,deleteCart);
-router.put("/updatecartquantity",protect,updateCartQuantity);
 router.put("/updateCart",protect,updateCart);
 
 
