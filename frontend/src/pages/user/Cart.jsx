7bbc62a677618db7b9017a64ecdcc2a5ca3c91ae
injifann <gutemaurgawa@gmail.com>
2026-07-15 15:19:19 +0300

    frontend:fixed bug in cart page

--- frontend/src/pages/user/Cart.jsx
+++ frontend/src/pages/user/Cart.jsx
@@ -137,7 +137,7 @@ export default function Cart() {
                                 {/* Product Details */}
                                 <div >
                                 <h3 className="text-lg font-semibold text-gray-800">
-                                    {item?.product.title}
+                                    {item?.product?.title}
                                 </h3>
 
                                 <p className="mt-1 text-sm text-gray-500">
